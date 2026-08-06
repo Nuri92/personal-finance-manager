65d0b299397198ef4c402b7bbf6d82a363facdde
Noor Ahmed Barekzai <nuri_nb@hotmail.de>
2026-08-07 00:44:58 +0200

    feat: load persisted expenses on application startup

--- src/main/java/de/nuri/personalfinancemanager/Main.java
+++ src/main/java/de/nuri/personalfinancemanager/Main.java
@@ -1,15 +1,23 @@
 package de.nuri.personalfinancemanager;
 
+import de.nuri.personalfinancemanager.model.Expense;
 import de.nuri.personalfinancemanager.repository.ExpenseRepository;
 import de.nuri.personalfinancemanager.repository.JsonExpenseRepository;
 import de.nuri.personalfinancemanager.service.ExpenseManager;
 
 import java.nio.file.Path;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
 		Path              filePath       = Path.of("data", "expenses.json");
 		ExpenseRepository repository     = new JsonExpenseRepository(filePath);
 		ExpenseManager    expenseManager = new ExpenseManager(repository);
+		
+		List<Expense> expenses = expenseManager.getAllExpenses();
+		System.out.println("Loaded expenses: " + expenses.size());
+		for (Expense e : expenses) {
+			System.out.println(e);
+		}
 	}
 }
